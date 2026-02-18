9cd01895c6e7d79b387ee13b17df131411323401
BLANCO ISAAC (AMXEXT) <isaac.blanco@ext.arcacontal.com>
2026-02-17 23:31:10 -0600

    feat: implement index page with dynamic content and structured sections

---
+++ app/pages/index.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import type { PageData } from '@@/types/pages/page';
+import type { ButtonProps } from '@nuxt/ui';
+
+const { data } = await useAsyncData('page', () => import(`@/assets/data/index.json`));
+const page = ref<PageData | undefined>(undefined);
+if (data.value) {
+  page.value = data.value as PageData;
+}
+const title = page.value?.seo?.title || page.value?.title;
+const description = page.value?.seo?.description || page.value?.description;
+const pageHeroLinks = page.value?.hero?.links as ButtonProps[];
+
+useSeoMeta({
+  titleTemplate: '',
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+});
+</script>
+
+<template>
+  <div v-if="page">
+    <UPageHero
+      :title="title"
+      :description="description"
+      :links="pageHeroLinks"
+    >
+      <template #top>
+        <HeroBackground />
+      </template>
+
+      <template #title>
+        <MDC
+          :value="page.title"
+          unwrap="p"
+        />
+      </template>
+
+      <PromotionalVideo />
+    </UPageHero>
+
+    <UPageSection
+      v-for="(section, index) in page.sections"
+      :key="index"
+      :title="section.title"
+      :description="section.description"
+      :orientation="section.orientation"
+      :reverse="section.reverse"
+      :features="section.features"
+    >
+      <ImagePlaceholder />
+    </UPageSection>
+
+    <UPageSection
+      :title="page.features.title"
+      :description="page.features.description"
+    >
+      <UPageGrid>
+        <UPageCard
+          v-for="(item, index) in page.features.items"
+          :key="index"
+          v-bind="item"
+          spotlight
+        />
+      </UPageGrid>
+    </UPageSection>
+
+    <UPageSection
+      id="testimonials"
+      :headline="page.testimonials.headline"
+      :title="page.testimonials.title"
+      :description="page.testimonials.description"
+    >
+      <UPageColumns class="xl:columns-4">
+        <UPageCard
+          v-for="(testimonial, index) in page.testimonials.items"
+          :key="index"
+          variant="subtle"
+          :description="testimonial.quote"
+          :ui="{ description: 'before:content-[open-quote] after:content-[close-quote]' }"
+        >
+          <template #footer>
+            <UUser
+              v-bind="testimonial.user"
+              size="lg"
+            />
+          </template>
+        </UPageCard>
+      </UPageColumns>
+    </UPageSection>
+
+    <USeparator />
+
+    <UPageCTA
+      v-bind="page.cta"
+      variant="naked"
+      class="overflow-hidden"
+    >
+      <LazyStarsBg />
+    </UPageCTA>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
