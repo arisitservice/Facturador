bea2547074fea77ea8c5251e4bbf41ab65d0d7eb
BLANCO ISAAC (AMXEXT) <isaac.blanco@ext.arcacontal.com>
2026-02-21 13:35:02 -0600

    feat: add AddNewRecordSlideover and client form components for new client registration

---
+++ app/components/catalogs/add-new-record-slideover.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { isAddNewRecordSlideoverOpen } = useDashboard();
+</script>
+
+<template>
+  <USlideover
+    v-model:open="isAddNewRecordSlideoverOpen"
+    title="Agregar nuevo registro"
+  >
+    <template #body>
+      <div>
+        <CatalogsFormsClients />
+      </div>
+    </template>
+  </USlideover>
+</template>
---
+++ app/components/catalogs/forms/clients.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import type { FormSubmitEvent } from '@nuxt/ui';
+
+import type { NewClient } from '~/lib/schemas/client';
+
+import { newClientSchema } from '~/lib/schemas/client';
+
+const { isAddNewRecordSlideoverOpen } = useDashboard();
+
+const state = ref<NewClient>({
+  name: '',
+  rfc: '',
+  postalCode: '',
+  taxRegime: '',
+  businessName: '',
+  accessApp: '',
+});
+
+const toast = useToast();
+async function onSubmit(event: FormSubmitEvent<NewClient>) {
+  toast.add({ title: 'Success', description: `New customer ${event.data.name} added`, color: 'success' });
+  isAddNewRecordSlideoverOpen.value = false;
+}
+</script>
+
+<template>
+  <UForm
+    :schema="newClientSchema"
+    :state="state"
+    class="space-y-4"
+    @submit="onSubmit"
+  >
+    <UFormField
+      label="Name"
+      placeholder="John Doe"
+      name="name"
+      required
+    >
+      <UInput v-model="state.name" class="w-full" />
+    </UFormField>
+    <UFormField
+      label="R.F.C"
+      placeholder="XAXX010101000"
+      name="rfc"
+      required
+    >
+      <UInput v-model="state.rfc" class="w-full" />
+    </UFormField>
+    <UFormField
+      label="Postal Code"
+      placeholder="12345"
+      name="postalCode"
+      required
+    >
+      <UInput v-model="state.postalCode" class="w-full" />
+    </UFormField>
+    <UFormField
+      label="Tax Regime"
+      placeholder="General"
+      name="taxRegime"
+      required
+    >
+      <UInput v-model="state.taxRegime" class="w-full" />
+    </UFormField>
+    <UFormField
+      label="Access App"
+      placeholder="App Name"
+      name="accessApp"
+      required
+    >
+      <UInput v-model="state.accessApp" class="w-full" />
+    </UFormField>
+    <div class="flex justify-end gap-2">
+      <UButton
+        label="Cancel"
+        color="neutral"
+        variant="subtle"
+        @click="isAddNewRecordSlideoverOpen = false"
+      />
+      <UButton
+        label="Create"
+        color="primary"
+        variant="solid"
+        type="submit"
+      />
+    </div>
+  </UForm>
+</template>
