82474528b2df1bd4dc7a50738c5109199c9b6a3e
BLANCO ISAAC (AMXEXT) <isaac.blanco@ext.arcacontal.com>
2026-02-24 13:48:56 -0600

    feat: add client data form and update new invoice page with client and tax info sections

--- app/pages/nova.vue
+++ app/pages/nova.vue
@@ -10,6 +10,32 @@ definePageMeta({
 
 const { isNotificationsSlideoverOpen, isAddNewRecordSlideoverOpen } = useDashboard();
 const route = useRoute();
+const title = computed(() => {
+  switch (route.name) {
+    case 'nova-dashboard':
+      return 'Home';
+    case 'nova-catalogs-clients':
+      return 'Clients';
+    case 'nova-catalogs-products-services':
+      return 'Products & Services';
+    case 'nova-catalogs-tax-regimes':
+      return 'Tax Regimes';
+    case 'nova-catalogs-invoice-usages':
+      return 'Invoice Usages';
+    case 'nova-catalogs-payment-methods':
+      return 'Payment Methods';
+    case 'nova-catalogs-payment_forms':
+      return 'Payment Forms';
+    case 'nova-billing-invoices-new-invoice':
+      return 'New Invoice';
+    case 'nova-billing-credit-notes-new-credit-note':
+      return 'New Credit Note';
+    case 'nova-billing-payment-complements-new-payment-complement':
+      return 'New Payment Complement';
+    default:
+      return '';
+  }
+});
 
 // show dashboard header and toolbar only on the dashboard index page
 const showDashboardHeader = computed(() => route.path === '/nova/dashboard');
@@ -25,7 +51,7 @@ const period = ref<Period>('daily');
 <template>
   <UDashboardPanel id="home">
     <template #header>
-      <UDashboardNavbar title="Home" :ui="{ right: 'gap-3' }">
+      <UDashboardNavbar title="ARIS TI NOVA" :ui="{ right: 'gap-3' }">
         <template #leading>
           <UDashboardSidebarCollapse />
         </template>
@@ -48,6 +74,9 @@ const period = ref<Period>('daily');
 
       <UDashboardToolbar>
         <template #left>
+          <h1 v-if="!showDashboardHeader">
+            {{ title }}
+          </h1>
           <div v-if="showDashboardHeader">
             <!-- NOTE: The `-ms-1` class is used to align with the `DashboardSidebarCollapse` button here. -->
             <DateRangePicker v-model="range" class="-ms-1" />
--- app/pages/nova/billing/invoices/new-invoice.vue
+++ app/pages/nova/billing/invoices/new-invoice.vue
@@ -1,13 +1,185 @@
 <script setup lang="ts">
+import type { NewInvoiceData } from '~/lib/schemas/billing';
+
+import { paymentForms, paymentMethods } from '~/lib/conts';
+import { clientDataSchema, newInvoiceDataSchema, productServiceSchema } from '~/lib/schemas/billing';
+
 definePageMeta({
   name: 'nova-billing-invoices-new-invoice',
+});
+
+const { data: clients } = await useBilling().client.getAllClients();
+
+const clientList = clients?.map((client, i) => ({
+  label: `client number ${i}`,
+  value: i,
+})) || [];
+
+console.log(clients);
+const state = ref<NewInvoiceData>({
+  client: {
+    clientId: 0,
+    businessName: '',
+    postalCode: '',
+    taxId: '',
+    taxRegimeId: 0,
+  },
+  taxInfo: {
+    invoiceUsageId: 0,
+    paymentCurrencyId: 0,
+    paymentMethod: '',
+    paymentForm: '',
+    relatedInvoiceId: undefined,
+  },
+  productServices: [],
+  vatDetails: {
+    vatChargedAtRate: false,
+    vatExempt: false,
+    vatWithholdingAtRate: false,
+    incomeTaxWithholding: false,
+    amount: 0,
+  },
 });
 </script>
 
 <template>
-  <div>
-    <h1>New Invoice</h1>
-  </div>
+  <UForm
+    :schema="newInvoiceDataSchema"
+    :state="state"
+    class="flex flex-col gap-8"
+  >
+    <!-- Client Data Form -->
+    <UForm
+      nested
+      name="client"
+      :schema="clientDataSchema"
+      class="flex flex-col gap-4"
+    >
+      <UFormField
+        label="Client"
+        name="clientId"
+        required
+      >
+        <USelect
+          v-model="state.client.clientId"
+          :items="clientList"
+        />
+      </UFormField>
+      <UFormField
+        label="Tax Regime"
+        name="taxRegimeId"
+        required
+      >
+        <UInput
+          disabled
+        />
+      </UFormField>
+      <UFormField
+        label="Tax ID"
+        name="taxId"
+        required
+      >
+        <UInput
+          disabled
+        />
+      </UFormField>
+      <UFormField
+        label="Business Name"
+        name="businessName"
+        required
+      >
+        <UInput
+          disabled
+        />
+      </UFormField>
+      <UFormField
+        label="Postal Code"
+        name="postalCode"
+        required
+      >
+        <UInput
+          disabled
+        />
+      </UFormField>
+    </UForm>
+    <!-- End Client Data Form -->
+
+    <!-- Tax Info Form -->
+    <UForm
+      nested
+      name="taxInfo"
+      class="flex flex-col gap-4"
+    >
+      <UFormField
+        label="Invoice Usage"
+        name="invoiceUsageId"
+      >
+        <UInput />
+      </UFormField>
+      <UFormField
+        label="Payment Method"
+        name="paymentMethod"
+      >
+        <USelect :items="paymentMethods" />
+      </UFormField>
+      <UFormField
+        label="Payment Form"
+        name="paymentForm"
+      >
+        <USelect :items="paymentForms" />
+      </UFormField>
+      <UFormField
+        label="Payment Currency"
+        name="paymentCurrencyId"
+      >
+        <UInput />
+      </UFormField>
+    </UForm>
+    <!-- End Tax Info Form -->
+
+    <UForm
+      nested
+      :schema="productServiceSchema"
+      name="productServices"
+      class="flex flex-col gap-4"
+    >
+      <UFormField
+        label="Detailed Description"
+        name="detailedDescription"
+      >
+        <UInput />
+      </UFormField>
+      <UFormField
+        label="Product/Service"
+        name="productServiceId"
+      >
+        <UInput />
+      </UFormField>
+      <UFormField
+        label="Measure unit"
+        name="measureUnit"
+      >
+        <UInput />
+      </UFormField>
+      <UFormField
+        label="Quantity"
+        name="quantity"
+      >
+        <UInput type="number" />
+      </UFormField>
+      <UFormField
+        label="Unit Price"
+        name="unitPrice"
+      >
+        <UInput type="number" />
+      </UFormField>
+      <UButton
+        label="Add Product/Service"
+        type="button"
+        @click="console.log('adding product');"
+      />
+    </UForm>
+  </UForm>
 </template>
 
 <style scoped>
