030467cbce77bb117aa6a4ede2ba2cbf68e9260c
BLANCO ISAAC (AMXEXT) <isaac.blanco@ext.arcacontal.com>
2026-02-25 00:20:19 -0600

    feat: add app configuration for UI components

---
+++ app/app.config.ts
@@ -0,0 +1,21 @@
+export default defineAppConfig({
+
+  ui: {
+    input: {
+      slots: {
+        base: 'w-full',
+      },
+    },
+    select: {
+      slots: {
+        base: 'w-full',
+      },
+    },
+    selectMenu: {
+      slots: {
+        base: 'w-full',
+      },
+    },
+  },
+
+});
