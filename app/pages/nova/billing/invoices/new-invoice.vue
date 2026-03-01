85d884dbd209d938731e0ced0d5a98bafdd32365
SValac <shiro.valac@gmail.com>
2026-02-28 20:57:36 -0600

    feat: enhance VAT details section with improved layout and functionality

--- app/pages/nova/billing/invoices/new-invoice.vue
+++ app/pages/nova/billing/invoices/new-invoice.vue
@@ -161,6 +161,45 @@ const state = ref<NewInvoiceData>({
             />
           </UFormField>
         </UForm>
+        <USeparator class="my-4" label="VAT Details" />
+        <UForm
+          nested
+          name="vatDetails"
+          class="flex gap-4"
+        >
+          <div class="flex flex-col md:flex-row gap-4">
+            <UFormField
+              label="VAT Rate"
+              name="vatChargedAtRate"
+            >
+              <USwitch v-model="state.vatDetails.vatChargedAtRate" @change="state.vatDetails.vatExempt = false" />
+            </UFormField>
+            <UFormField
+              label="VAT Exempt"
+              name="vatExempt"
+            >
+              <USwitch v-model="state.vatDetails.vatExempt" @change="state.vatDetails.vatChargedAtRate = false" />
+            </UFormField>
+            <UFormField
+              label="VAT Retention Rate"
+              name="vatWithholdingAtRate"
+            >
+              <USwitch v-model="state.vatDetails.vatWithholdingAtRate" />
+            </UFormField>
+            <UFormField
+              label="Tax Retention Rate"
+              name="incomeTaxWithholding"
+            >
+              <USwitch v-model="state.vatDetails.incomeTaxWithholding" />
+            </UFormField>
+            <UFormField
+              label="Amount"
+              name="amount"
+            >
+              <UInput type="number" class="w-full" />
+            </UFormField>
+          </div>
+        </UForm>
       </UCard>
       <!-- End Tax Info Form -->
       <!-- Product/Service Form -->
@@ -220,49 +259,6 @@ const state = ref<NewInvoiceData>({
       </UCard>
     </div>
     <UTable :data="dummyTableData" class="flex-1" />
-    <UCard>
-      <template #header>
-        <h2 class="text-xl lg:text-2xl font-bold">
-          VAT Details
-        </h2>
-      </template>
-      <UForm
-        nested
-        name="vatDetails"
-        class="flex gap-4"
-      >
-        <UFormField
-          label="VAT Charged at Rate"
-          name="vatChargedAtRate"
-        >
-          <USwitch v-model="state.vatDetails.vatChargedAtRate" @change="state.vatDetails.vatExempt = false" />
-        </UFormField>
-        <UFormField
-          label="VAT Exempt"
-          name="vatExempt"
-        >
-          <USwitch v-model="state.vatDetails.vatExempt" @change="state.vatDetails.vatChargedAtRate = false" />
-        </UFormField>
-        <UFormField
-          label="VAT Withholding at Rate"
-          name="vatWithholdingAtRate"
-        >
-          <USwitch v-model="state.vatDetails.vatWithholdingAtRate" />
-        </UFormField>
-        <UFormField
-          label="Income Tax Withholding"
-          name="incomeTaxWithholding"
-        >
-          <USwitch v-model="state.vatDetails.incomeTaxWithholding" />
-        </UFormField>
-        <UFormField
-          label="Amount"
-          name="amount"
-        >
-          <UInput type="number" class="w-full" />
-        </UFormField>
-      </UForm>
-    </ucard>
   </UForm>
 </template>
 
